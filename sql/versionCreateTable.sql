9f72e134facaaed509dcc062bfb96746af1fa5fc
Tom Watson <thomas.watson@here.com>
2018-10-29 14:16:09 -0700

    Add title to version

--- sql/versionCreateTable.sql
+++ sql/versionCreateTable.sql
@@ -1,6 +1,7 @@
 CREATE TABLE version (
  version_id UUID PRIMARY KEY,
  gist_id UUID NOT NULL REFERENCES gist (gist_id),
+ title STRING,
  created_on TIMESTAMP NOT NULL,
  prior_version_id UUID REFERENCES version (version_id)
 );
