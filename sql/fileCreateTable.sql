531fa07900ea05a3b1377599bb5541845b2295aa
Tom Watson <thomas.watson@here.com>
2018-11-06 07:57:31 -0800

    Finished openAPI def

--- sql/fileCreateTable.sql
+++ sql/fileCreateTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE file (
  file_id UUID PRIMARY KEY,
- gist_id UUID NOT NULL REFERENCES gist (gist_id),
  created_on TIMESTAMP NOT NULL,
  content TEXT,
  langauge TEXT NOT NULL,
- file_name TEXT
+ file_name TEXT,
+ prior_file_id UUID REFERENCES file (file_id)
 );
